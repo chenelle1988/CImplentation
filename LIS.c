db8d52d7ecb4349c558769811aa30c5509e4402b
Lanner <zcalien.g@gmail.com>
2013-06-06 17:36:10 +0800

    根据编程之美提供的解法一修改LIS

--- LIS.c
+++ LIS.c
@@ -8,76 +8,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+typedef struct _node {
+    int length;
+    int pre;
+} Node;
 
 #define MAX 8
 
-int DP[MAX][MAX];       //DP[i][j]代表array[0...i]的子序列中最大值不超过sortedArray[j]的最长递增子序列的长度
-
-void quickSort(int *array, int s, int e);
-int partition(int *array, int s, int e);
-void calcDPMatrix(int *array, int *sortedArray);
+void calcDP(int *array, Node *DP, int size);
+int maxElement(int *array, int size);
+void printLIS(int *array, Node *LLIS, int size);
 
 int main()
 {
     int array[MAX] = {8, 3, 7, 5, 1, 2, 9, 0};
+    Node LLIS[MAX];
+    calcDP(array, LLIS, MAX);
+    printLIS(array, LLIS, MAX);
     
-    int sortedArray[MAX];
-    int i;
+    return 0;
+}
+
+void calcDP(int *array, Node *LLIS, int size)
+{
+    int i, j;
     for (i = 0; i < MAX; i++) {
-        sortedArray[i] = array[i];
-    }
-    quickSort(sortedArray, 0, MAX-1);
-    
-    calcDPMatrix(array, sortedArray);
-    printf("Length of LIS is %d", DP[MAX-1][MAX-1]);
-    
-	return 0;
-}
-
-void calcDPMatrix(int *array, int *sortedArray)
-{
-    /**
-     * 利用递归式计算DP每一项的值
-     * DP[i][j] = MAX(DP[i-1][j], array[i] > sortedArray[j] ? DP[i][j-1]+1 : DP[i-1][j])
-     * 递归式解释：
-     * array[0...i]的最大递增子序列可能是
-     * 1. array[0...i-1]的最大递增子序列(长度为array[0...i-1]的最大递增子序列的长度；
-     * 2. array[0...i-1]的最大递增子序列加上array[i]，长度为array[0...i-1]的最大递增子序列的长度加1
-     * 但第二种情况有个条件，就是array[i]要比array[0...i-1]的最大递增子序列的最大值要大。
-     */
-}
-
-void quickSort(int *array, int s, int e)
-{
-    if (s < e) {
-        int mid = partition(array, s, e);
-        quickSort(array, s, mid-1);
-        quickSort(array, mid+1, e);
+        LLIS[i].length = 1;
+        LLIS[i].pre = -1;
+        for (j = 0; j < i; j++) {
+            if (array[i] >= array[j] && LLIS[j].length+1 > LLIS[i].length) {
+                LLIS[i].length = LLIS[j].length+1;
+                LLIS[i].pre = j;
+            }
+        }
     }
 }
 
-int partition(int *array, int s, int e)
+void printLIS(int *array, Node *LLIS, int size)
 {
-    int i = s, j = e;
-    int key = array[i];
-    while (i < j) {
-        while (i < j && array[j] >= key) {
-            j--;
-        }
-        if (i < j) {
-            array[i] = array[j];
-            i++;
-        }
-        while (i < j && array[i] <= key) {
-            i++;
-        }
-        if (i < j) {
-            array[j] = array[i];
-            j--;
+    int lastNode = -1;
+    Node tmpNode;
+    int i;
+    for (i = 0; i < size; i++) {
+        tmpNode = LLIS[i];
+        if (tmpNode.pre != -1) {
+            lastNode = i;
         }
     }
     
-    array[i] = key;
-    
-    return i;
+    while (lastNode != -1) {
+        printf("%d ", array[lastNode]);
+        tmpNode = LLIS[lastNode];
+        lastNode = tmpNode.pre;
+    }
 }
