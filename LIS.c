a7a5cce1581d98ddb40b5c490ccb8c20866e0a0a
Lanner <zcalien.g@gmail.com>
2013-06-08 17:27:53 +0800

    在编程之美给出的方法一的思路的基础上，增加函数，打印出所有的最长递增子序列

--- LIS.c
+++ LIS.c
@@ -10,16 +10,22 @@
 #include <stdlib.h>
 #include <limits.h>
 
+#define MAX 8
+
+typedef struct _intArray {
+    int count;
+    int elements[MAX];
+} IntArray;
+
 typedef struct _node {
     int length;
-    int pre;
+    IntArray preNodes;
 } Node;
-
-#define MAX 8
 
 void calcDP(int *array, Node *DP, int size);
 int maxElement(int *array, int size);
 void printLIS(int *array, Node *LLIS, int size);
+void printLISFromNode(Node *LLIS, int lastNode);
 
 int main()
 {
@@ -36,13 +42,21 @@ void calcDP(int *array, Node *LLIS, int size)
     int i, j;
     for (i = 0; i < MAX; i++) {
         LLIS[i].length = 1;
-        LLIS[i].pre = -1;
+        int count = 0;
         for (j = 0; j < i; j++) {
             if (array[i] >= array[j] && LLIS[j].length+1 > LLIS[i].length) {
-                LLIS[i].length = LLIS[j].length+1;
-                LLIS[i].pre = j;
+                if (LLIS[j].length+1 > LLIS[i].length) {
+                    count = 0;
+                    LLIS[i].length = LLIS[j].length+1;
+                    LLIS[i].preNodes.elements[count] = j;
+                    count++;
+                } else if (LLIS[j].length+1 == LLIS[i].length) {
+                    LLIS[i].preNodes.elements[count] = j;
+                    count++;
+                }
             }
         }
+        LLIS[i].preNodes.count = count;
     }
 }
 
@@ -53,14 +67,15 @@ void printLIS(int *array, Node *LLIS, int size)
     int i;
     for (i = 0; i < size; i++) {
         tmpNode = LLIS[i];
-        if (tmpNode.pre != -1) {
+        if (tmpNode.preNodes.count != 0) {
             lastNode = i;
         }
     }
     
-    while (lastNode != -1) {
-        printf("%d ", array[lastNode]);
-        tmpNode = LLIS[lastNode];
-        lastNode = tmpNode.pre;
-    }
+    printLISFromNode(LLIS, lastNode);
+}
+
+void printLISFromNode(Node *LLIS, int lastNode)
+{
+    
 }
